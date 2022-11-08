a435ea6156fb359b4af716ea6768b5c7e79a36c8
michaelleojacob <michaelleojacob@gmail.com>
2022-11-08 15:13:58 -0800

    +put &deg; in spans and gave them all display:absolute. +colors for low and high temps. *sizing of current weather card

--- src/app/components/currentWeather/current.js
+++ src/app/components/currentWeather/current.js
@@ -1,5 +1,5 @@
-import mockCurrent from '../../utils/mockCurrent';
-import convertTemp from '../../utils/convertTemp';
+import mockCurrent from "../../utils/mockCurrent";
+import convertTemp from "../../utils/convertTemp";
 
 const CurrentWeather = ({ currentWeather, isImperial }) => {
   const { main } = mockCurrent;
@@ -7,22 +7,32 @@ const CurrentWeather = ({ currentWeather, isImperial }) => {
   const mainTemp = convertTemp(main.temp);
   const lowTemp = convertTemp(main.temp_min);
   const highTemp = convertTemp(main.temp_max);
-  const feelsLike = convertTemp(main.feels_like);
+  // const feelsLike = convertTemp(main.feels_like);
 
   return (
-    <div className='current-container'>
+    <div className="current-container">
       <div>{mockCurrent.name}</div>
-      <div>{mockCurrent.weather[0].main}</div>
-      <img
+      {/* <img
+        className="current-icon"
         src={`https://openweathermap.org/img/wn/${mockCurrent.weather[0].icon}@2x.png`}
-        alt='weather icon'
-      />
-      <div>{isImperial ? mainTemp[0] : mainTemp[1]}&deg;</div>
-      <div>
-        <div>L {isImperial ? lowTemp[0] : lowTemp[1]}&deg;</div>
-        <div>H {isImperial ? highTemp[0] : highTemp[1]}&deg;</div>
+        alt="weather icon"
+      /> */}
+      <div className="current-main">
+        {isImperial ? mainTemp[0] : mainTemp[1]}
+        <span className="deg">&deg;</span>
       </div>
-      <div>feels like: {isImperial ? feelsLike[0] : feelsLike[1]}&deg;</div>
+      <div>{mockCurrent.weather[0].main}</div>
+      <div className="current-low-hi">
+        <div className="low">
+          L:{isImperial ? lowTemp[0] : lowTemp[1]}
+          <span className="deg">&deg;</span>
+        </div>
+        <div className="high">
+          H:{isImperial ? highTemp[0] : highTemp[1]}
+          <span className="deg">&deg;</span>
+        </div>
+      </div>
+      {/* <div>feels like: {isImperial ? feelsLike[0] : feelsLike[1]}<span className='deg'>&deg;</span></div> */}
     </div>
   );
 };
--- src/app/components/forcast/forcastCards/forcastCards.js
+++ src/app/components/forcast/forcastCards/forcastCards.js
@@ -1,43 +1,37 @@
-import LowHiAvg from '../../../utils/getLowHighAvg';
-import convertTemp from '../../../utils/convertTemp';
+import LowHiAvg from "../../../utils/getLowHighAvg";
+import convertTemp from "../../../utils/convertTemp";
 
-const weekday = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const ForcastCard = ({ dt_txt, list, isImperial }) => {
   const mid = Math.floor(list.length / 2);
   const numericDay = new Date(list[mid].dt_txt).getDay();
   const dayOfWeek = weekday[numericDay];
-  const { low, high, avg, icon } = LowHiAvg(list);
+  const { low, high, icon } = LowHiAvg(list);
 
   const lowTemp = convertTemp(low);
   const highTemp = convertTemp(high);
-  const avgTemp = convertTemp(avg);
+  // const avgTemp = convertTemp(avg);
 
   return (
-    <div className='forcast-card'>
+    <div className="forcast-card">
       <div>{dayOfWeek}</div>
       <img
-        className='forcast-card-icon'
+        className="forcast-card-icon"
         src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-        alt='weather icon'
+        alt="weather icon"
       />
-      <div className='forcast-card-temp-container'>
-        <div className='forcast-card-temp'>
-          {isImperial ? lowTemp[0] : lowTemp[1]}&deg;
+      <div className="forcast-card-temp-container">
+        <div className="forcast-card-temp low">
+          {isImperial ? lowTemp[0] : lowTemp[1]}
+          <span className="deg">&deg;</span>
         </div>
         {/* <div className='forcast-card-temp'>
-          {isImperial ? avgTemp[0] : avgTemp[1]}&deg;
+          {isImperial ? avgTemp[0] : avgTemp[1]}<span className='deg'>&deg;</span>
         </div> */}
-        <div className='forcast-card-temp'>
-          {isImperial ? highTemp[0] : highTemp[1]}&deg;
+        <div className="forcast-card-temp high">
+          {isImperial ? highTemp[0] : highTemp[1]}
+          <span className="deg">&deg;</span>
         </div>
       </div>
     </div>
