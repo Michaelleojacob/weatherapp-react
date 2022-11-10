afb3d0385358bfd1d2c4a8e1543d6680a4f99813
migs <michaelleojacob@gmail.com>
2022-11-10 15:23:56 -0800

    -switch components I no longer use

--- src/app/components/switch/celFahSwitch.js
+++ src/app/components/switch/celFahSwitch.js
@@ -1,13 +1,13 @@
-import "./switch.css";
-import { Switch } from "@mui/material";
+import './switch.css';
+import { Switch } from '@mui/material';
 
 const CelFahSwitch = ({ toggleImperial }) => {
   return (
-    <>
+    <div className='switch-container'>
       <label>C</label>
-      <Switch size="large" onChange={toggleImperial} />
+      <Switch size='large' onChange={toggleImperial} />
       <label>F</label>
-    </>
+    </div>
   );
 };
 
